9fe665329a10380d7f8402448d1a6229a7395dd6
Evan Lemire <elemire@broadinstitute.org>
2017-11-03 14:33:54 -0400

    Lates version of cut to L2

--- cut_to_l2.py
+++ cut_to_l2.py
@@ -7,10 +7,10 @@ def cut_l1(original_list):
         files = [x for x in original_list if os.path.basename(x).startswith(pert)]
         plate_names = [os.path.basename(x) for x in files]
         replicate_ids = [x.split("_")[2] for x in plate_names]
-        short_reps = [x[:2] for x in replicate_ids]
+        #short_reps = [x[:2] for x in replicate_ids]
         keep = []
 
-        for r in set(short_reps):
+        for r in set(replicate_ids):
             temp = [y for y in replicate_ids if y.startswith(r)]
 
             if len(temp) == 1:
@@ -19,9 +19,13 @@ def cut_l1(original_list):
 
                 temp2 = [z for z in temp if "." in z]
 
+                if len(temp2) ==0:
+                    import pdb
+                    pdb.set_trace()
                 max_l = max([int(x[-1]) for x in temp2])
                 temp3 = [b for b in temp2 if b.endswith(str(max_l))]
                 keep.append(temp3[0])
+
         keep_perts = [x for x in plate_names if x.split('_')[2] in keep]
         keep_files = [path for path in files if os.path.basename(path) in keep_perts]
         [new_list.append(x) for x in keep_files]
