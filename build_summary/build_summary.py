0b766e69c995a9f56d6590edae2b802e1edf1432
Jacob Asiedu <jasiedu@broadinstitute.org>
2019-02-14 13:37:43 -0500

    Adding QC script

--- build_summary/build_summary.py
+++ build_summary/build_summary.py
@@ -14,6 +14,8 @@ import logging
 import expected_sensitivities as expected_sense
 import count_heatmap as c_map
 import argparse
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import sc_plot
