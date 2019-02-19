466c20af07a4b9334eade89772467f1a7906c6c4
Evan Lemire <elemire@broadinstitute.org>
2019-02-19 10:50:01 -0500

    Fixing failed plates bug

--- normalization/mk_build_file.py
+++ normalization/mk_build_file.py
@@ -107,11 +107,8 @@ def mk_cell_metadata(args, failed_plates):
                            row_metadata_df=pd.DataFrame(index=ssmd_mat.index))
     wg.write(ssmd_gct, os.path.join(args.build_folder, args.cohort_name + '_ssmd_matrix_n{}_{}.gct'.format(ssmd_gct.data_df.shape[1], ssmd_gct.data_df.shape[0])))
 
-    import pdb
-    pdb.set_trace()
-    
     ssmd_failures = ssmd_gct.data_df.median()[ssmd_gct.data_df.median() < 2].index.tolist()
-    pd.DataFrame({'dropout_failures': failed_plates[1], 'ssmd_failures': pd.Series(ssmd_failures)}).to_csv(os.path.join(args.build_folder, 'failed_plates.txt'), sep='\t', index=False)
+    pd.DataFrame({'dropout_failures': failed_plates, 'ssmd_failures': pd.Series(ssmd_failures)}).to_csv(os.path.join(args.build_folder, 'failed_plates.txt'), sep='\t', index=False)
 
 def mk_inst_info(inst_data, norm_data, args):
 
