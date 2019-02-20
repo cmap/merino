7b647ad624467323217b61c4e82529afa8cb39d2
Evan Lemire <elemire@broadinstitute.org>
2019-02-20 11:04:15 -0500

    editing setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import setuptools
 
-setuptools.setup(name="merino", packages=["merino"])
+setuptools.setup(name="merino", packages=["assemble", "build_summary", "card", "misc_tools", "normalization"])
 
