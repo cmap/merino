1f40b30f014aba6eda7b013390db56772f12505f
Evan Lemire <elemire@broadinstitute.org>
2019-05-16 10:20:27 -0400

    Updating quick qc to allow for replicate plate maps

--- merino/build_summary/utils/quick_qc.py
+++ merino/build_summary/utils/quick_qc.py
@@ -15,13 +15,14 @@ logger = logging.getLogger(setup_logger.LOGGER_NAME)
 def build_parser():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-plate_name', '-p', help='name of plate to process up to card and run plate qc')
+    parser.add_argument('-rep_map', '-rep', action="store_true", help='name of plate to process up to card and run plate qc')
 
     return parser
 
 def main(args):
     plate_entry = parse_plate_name(args.plate_name)
 
-    (project_dir, jcsv_path, plate_map_path, assemble_out_path, qc_out_path) = build_paths(plate_entry)
+    (project_dir, jcsv_path, plate_map_path, assemble_out_path, qc_out_path) = build_paths(plate_entry, args.rep_map)
 
     assemble_args = assemble.build_parser().parse_args(['-pmp', plate_map_path, '-csv', jcsv_path, '-out', assemble_out_path])
     logger.info("Running assemble with args: {}".format(assemble_args))
@@ -35,6 +36,7 @@ def main(args):
 
     plate_qc_args = plate_qc.build_parser().parse_args(['-project_folder', project_dir, '-plate_name', args.plate_name,
                                                         '-qc', qc_out_path, '-gmt', gmt_path])
+
     logger.info("Running PlateQC with args: {}".format(plate_qc_args))
     plate_qc.main(plate_qc_args)
 
@@ -42,13 +44,21 @@ def main(args):
 def parse_plate_name(plate_name):
     pieces = plate_name.split("_")
     pert_plate = pieces[0]
+    rep_id = pieces[3]
     project_code = pert_plate.translate(None, digits)
-    return {"pert_plate": pert_plate, "project_code": project_code, "det_plate": plate_name}
 
-def build_paths(plate_entry):
+    return {"pert_plate": pert_plate, "project_code": project_code, "det_plate": plate_name, "rep_id": rep_id}
+
+def build_paths(plate_entry, rep_map):
     project_dir = os.path.join('/cmap/obelix/pod/custom/', plate_entry['project_code'])
     jcsv_path = os.path.join(project_dir, 'lxb', plate_entry['det_plate'], plate_entry['det_plate'] + '.jcsv')
-    plate_map_path = os.path.join(project_dir, 'map_src', plate_entry['pert_plate'] + '.src')
+
+    if rep_map == False:
+        plate_map_path = os.path.join(project_dir, 'map_src', plate_entry['pert_plate'] + '.src')
+
+    elif rep_map == True:
+        plate_map_path = os.path.join(project_dir, 'map_src', plate_entry['pert_plate'] + '.' + plate_entry['rep_id'] + '.src')
+
     assemble_out_path = project_dir
     qc_out_path = os.path.join(project_dir, 'qc')
     if not os.path.exists(qc_out_path):
