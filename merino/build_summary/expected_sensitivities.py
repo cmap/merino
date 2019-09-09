b00a8093937606f8e768b16f37de44870580146d
Evan Lemire <elemire@broadinstitute.org>
2019-09-09 15:42:43 -0400

    changing import of gmt from pestle to cmappy

--- merino/build_summary/expected_sensitivities.py
+++ merino/build_summary/expected_sensitivities.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import merino.setup_logger as setup_logger
 import numpy as np
 import pandas as pd
-import pestle.io.gmt as gmt
+import cmapPy.set_io.gmt as gmt
 from statsmodels.distributions.empirical_distribution import ECDF
 
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
