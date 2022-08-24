9b5e9f35d5d37c8366d940f5d706577d2edef915
Anup Jonchhe <APJonchhe@gmail.com>
2022-08-24 13:46:56 -0400

    added reminder to push changes before building docker container

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+
 #change the version number for each new build
+# Note: You must push changes to Github for them to appear in the docker container
 docker build --platform linux/amd64 --no-cache -t prismcmap/merino:latest -t prismcmap/merino:v0.1.5 --rm=true .
